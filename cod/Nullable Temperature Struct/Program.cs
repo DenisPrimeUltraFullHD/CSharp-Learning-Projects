c3a5570b2fb541bed8ad9506ce9e33a4df66a4ca
Denis <matrd0960@gmail.com>
2025-06-02 12:13:10 +0300

    Update Program.cs

--- cod/Nullable Temperature Struct/Program.cs
+++ cod/Nullable Temperature Struct/Program.cs
@@ -36,15 +36,14 @@ class Program
 
     static void Main()
     {
-        var t1 = new Temperature(23.456);   // DegreesCelsius = 23.456
-        Console.WriteLine(t1.HasValue());   // True
-        Console.WriteLine(t1.ToFahrenheit()); // ≈ 74.2
-        Console.WriteLine(t1);              // "23.5 °C"
+        var t1 = new Temperature(23.456);  
+        Console.WriteLine(t1.HasValue());   
+        Console.WriteLine(t1.ToFahrenheit()); 
+        Console.WriteLine(t1);           
 
-        var t2 = new Temperature(null);     // DegreesCelsius = null
-        Console.WriteLine(t2.HasValue());   // False
-        Console.WriteLine(t2);              // "—"
-                                            // t2.ToFahrenheit(); // бросит InvalidOperationException
+        var t2 = new Temperature(null);     
+        Console.WriteLine(t2.HasValue());   
+        Console.WriteLine(t2);             
     }
 }
 }
